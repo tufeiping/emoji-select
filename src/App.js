4b170091c0ea86e3c83d90478d0369ca5114afd6
sunny <tufeiping@hotmail.com>
2024-10-05 16:37:19 +0800

    # modify some UI view

--- src/App.js
+++ src/App.js
@@ -6,8 +6,38 @@ function App() {
   return (
     <div className="App">
       <EmojiSelector />
+      <footer className="footer" style={footerStyle}>
+        <p style={footerTextStyle}>
+          GitHub 仓库：
+          <a href="https://github.com/tufeiping/emoji-select" target="_blank" rel="noopener noreferrer" style={linkStyle}>
+            <img src="https://img.shields.io/badge/GitHub-Repository-blue?style=flat&logo=github" alt="GitHub" />
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
+
+const footerStyle = {
+  position: 'fixed', // 固定在底部
+  bottom: '0',
+  left: '0',
+  right: '0',
+  padding: '10px',
+  textAlign: 'center',
+  backgroundColor: '#f8f9fa', // 背景颜色
+  borderTop: '1px solid #e7e7e7', // 上边框
+  zIndex: '1000', // 确保在其他内容之上
+};
+
+const footerTextStyle = {
+  margin: '0',
+  color: '#333', // 字体颜色
+  fontSize: '12px', // 设置字体大小
+};
+
+const linkStyle = {
+  marginLeft: '5px', // 链接与文本之间的间距
+};
 
 export default App;
